8e2123f6fa5e95f2255bc0a5f9f209fdcaba47b2
Prince Chauda <prince.chauda@gmail.com>
2021-05-14 00:40:53 -0400

    Initial commit

---
+++ src/main/java/com/example/restservice/model/Greeting.java
@@ -0,0 +1,14 @@
+package com.example.restservice.model;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Value;
+
+@Value
+@Builder
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class Greeting {
+    long id;
+    String content;
+}
